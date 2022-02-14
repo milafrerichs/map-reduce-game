09df26aeefe1f7f089bac8442d4e23d539d77821
Yonga Springfield <SPRINGFIELDYONGA@OUTLOOK.COM>
2022-02-14 10:59:34 +0100

    11 reset button (#13)
    * added store
    * add restart button
    
    Co-authored-by: Mila Frerichs <mila.frerichs@gmail.com>

--- src/Answer.svelte
+++ src/Answer.svelte
@@ -1,24 +1,32 @@
 <script>
-  import { checkData, getResults } from './utils/data.js';
-  import Table from './Table.svelte';
+  import { getResults } from "./utils/data.js";
+  import Table from "./Table.svelte";
 
-  export let data = []
+  export let data = [];
   export let answer = {};
-  export let question = {}
-    export let columns = [
-        {key: 'island', name: 'Island'},
-        {key: 'month', name: 'Month'},
-        {key: 'temp', name: 'Temperature in °C'},
-      ]
+  export let question = {};
+  export let columns = [
+    { key: "island", name: "Island" },
+    { key: "month", name: "Month" },
+    { key: "temp", name: "Temperature in °C" },
+  ];
   let answers = [];
 
-  $: if(data) {
-    answers = getResults(data, question)
+  $: if (data) {
+    answers = getResults(data, question);
+  }
+
+    function handleClick(){
+      window.dispatchEvent(
+        new CustomEvent(`mrGame:restart`, { bubbles: true })
+      );
   }
 </script>
+
 <div class="m-auto max-w-7xl h-full">
   <h3 class="text-2xl mb-4">{question.text}</h3>
   {#if answers.length > 0}
     <Table data={answers} {columns} />
   {/if}
+  <button on:click={handleClick}>Restart</button>
 </div>

4bf9533bbdaae2e219fa3a8fd52c219406994185
Yonga Springfield <springfieldyonga@outlook.com>
2022-02-19 10:20:34 +0100

    created check answer

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,21 +1,22 @@
 
 export function checkData(data, question, answer) {
   let filteredData = data.filter(question.filter)
-  let result = filteredData.reduce((a,b) => {
+  let result = filteredData.reduce((a, b) => {
     return question.typeFunction(a, b[question.column]);
   }, 0)
   return result === answer;
 }
+
 export function getEntry(data, question) {
   const result = getResults(data, question);
-  if(result.length > 0) {
+  if (result.length > 0) {
     return result[0]
   }
 }
 
 export function getResults(data, question) {
   let filteredData = data.filter(question.filter)
-  let result = filteredData.reduce((a,b) => {
+  let result = filteredData.reduce((a, b) => {
     return question.typeFunction(a, b[question.column]);
   }, 0)
   return filteredData.filter((d) => {
@@ -23,9 +24,19 @@ export function getResults(data, question) {
   })
 }
 
-export function checkAnswer() {
-  
+
+export function checkAnswer(data, question, answer = '') {
+  const entry = getEntry(data, question);
+  const { island, temp } = entry;
+  const answerLength = `${island}:${temp}`.length
+  const regex = new RegExp(`(${island})(?:.*)(${temp})`, 'i');
+  const matches = regex.test(answer)
+  return {
+    isCorrectAnswer: matches && answer.length === answerLength,
+    entry
+  };
 }
+
 
 export const monthNames = [
   "January",

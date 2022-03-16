e904586baecd53a77b2642e2cff53eb2e809913a
Mila Frerichs <mila.frerichs@gmail.com>
2022-03-16 11:50:42 +0100

    feat: add waves and gradient for seasons

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import Data from "./Data.svelte";
+  import Waves from "./Waves.svelte";
   import Question from "./Question.svelte";
   import Answers from "./Answers.svelte";
   import Result from "./Result.svelte";
@@ -47,6 +48,11 @@
     gameSettings.set(settings);
   }
 
+  function backgroundGradien() {
+
+      return 
+  }
+
   onMount(() => {
     window.addEventListener("mrGame:next", (e) => next());
     window.addEventListener("mrGame:prev", (e) => prev());
@@ -63,8 +69,10 @@
     <Data {width} {height} />
   {:else if $stepIndex === 4}
     <Question />
+    <Waves />
   {:else if $stepIndex === 5}
     <Answers />
+    <Waves />
   {:else}
     <Result />
   {/if}
@@ -74,4 +82,36 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+  :global(.ocean) {
+    background: linear-gradient(-45deg, #587bba, #004567, #22699c, #68ACE1);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    height: 100vh;
+  }
+  :global(.autum) {
+    background: rgb(217,221,228);
+    background: linear-gradient(180deg, rgba(217,221,228,1) 0%, rgba(243,199,181,1) 43%, rgba(251,181,147,1) 100%);
+  }
+  :global(.summer) {
+    background: linear-gradient(180deg, #BD8CD0 0%, #F19183 48.93%, #FFC975 100%);
+  }
+  :global(.winter) {
+    background: linear-gradient(180deg, #3956A4 0%, #587BBA 48.93%, #7990C1 100%);
+  }
+  :global(.spring) {
+    background: linear-gradient(180deg, #BCDAFC 0%, #A1CFF3 48.93%, #68ACE1 100%);
+  }
+
+  @keyframes gradient {
+    0% {
+              background-position: 0% 50%;
+    }
+    50% {
+              background-position: 100% 50%;
+    }
+    100% {
+              background-position: 0% 50%;
+    }
+  }
+
 </style>
